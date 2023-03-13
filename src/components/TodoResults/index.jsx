aa5541ec6c4aa18643d6a033653464c4cc1dfc1b
NataGalvez <nataliac.galvezc@gmail.com>
2023-03-13 11:20:45 -0300

    feat: finished todo counter

--- src/components/TodoResults/index.jsx
+++ src/components/TodoResults/index.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
-
+import { useDispatch, useSelector } from "react-redux";
 const TodoResults = () => {
+  const { todos } = useSelector((state) => state.data);
   // Fix an ability to calculate completed tasks
 
-  return <div className="todo-results">Done:</div>;
+  return (
+    <div className="todo-results">
+      Done:
+      {
+        todos
+          .map((element) => element.checked)
+          .filter((element) => element === true).length
+      }
+    </div>
+  );
 };
 
 export default TodoResults;
