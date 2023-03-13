a754a557ab51501be9e1be49608a9ec928c2e4f7
NataGalvez <nataliac.galvezc@gmail.com>
2023-03-13 02:01:01 -0300

    chore: wrap the aplication with redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {ApiProvider} from '@reduxjs/toolkit/query/react'
-import { apiSlice } from "./slices/apiSlice";
+import { Provider } from "react-redux";
+import store from "./store/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  
   <React.StrictMode>
-    <ApiProvider api={apiSlice} >
-    <App />
-    </ApiProvider>
-   </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
