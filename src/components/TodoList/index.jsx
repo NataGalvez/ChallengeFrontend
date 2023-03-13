e0354725201e682efdcf6bcdddeaedbc675a652c
NataGalvez <nataliac.galvezc@gmail.com>
2023-03-13 02:02:18 -0300

    feat: list todo's and multiple functions

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
-import { useGetTodoQuery } from "../../slices/apiSlice";
 import TodoListItem from "../TodoListItem";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  deleteTodos,
+  loadTodos,
+  getTodos,
+  updateTodos,
+} from "../../store/actions";
 const TodoList = () => {
-  const { data: todo, isError, isLoading, isSuccess } = useGetTodoQuery();
-  const handleDelete = (todoId) => {
-    // Fix an ability to delete task
-  };
+  const dispatch = useDispatch();
+  const { todos } = useSelector((state) => state.data);
 
   const toggleCheck = (todoId, isChecked) => {
     // Fix an ability to toggle task
+    let newTodos = [...todos];
+    let todo = newTodos.find((todo) => todo.id === todoId);
+    todo.checked = !isChecked;
+    dispatch(updateTodos(todoId));
   };
-  console.log(todo);
+
+  useEffect(() => {
+    dispatch(loadTodos());
+  }, []);
+  const handleDelete = (todoId) => {
+    // Fix an ability to delete task
+
+    dispatch(deleteTodos(todoId));
+    dispatch(getTodos(todos.filter((todo) => todo.id !== todoId)));
+  };
+
   return (
     <div className="todo-list">
       <span className="todo-list-title">Things to do:</span>
-      {!isLoading &&
-        isSuccess &&
-        todo.map((element) => {
+      {todos &&
+        todos.map((todo) => {
           return (
-            <div className="todo-list-content">
-              <TodoListItem label={element.label} />
+            <div className="todo-list-content" key={todo.id}>
+              <TodoListItem
+                label={todo.label}
+                onDelete={() => handleDelete(todo.id)}
+                checked={todo.checked}
+                onCheck={() => toggleCheck(todo.id, todo.checked)}
+              />
             </div>
           );
         })}
-      {!todo && (
+      {!todos && (
         <div className="no-todos">
           Looks like you&apos;re absolutely free today!
         </div>
